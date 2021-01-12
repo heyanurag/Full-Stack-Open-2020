bcef9675d8063832035f746b923b09eddb9d8af9
Anurag Singh <sa.anuragsingh@gmail.com>
2021-01-12 20:48:03 +0530

    4.2 complete

--- Part4/bloglist/app.js
+++ Part4/bloglist/app.js
@@ -2,10 +2,10 @@ const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
 const mongoose = require('mongoose')
-const Blog = require('./models/blog')
 const logger = require('./utils/logger')
-//const middleware = require('./utils/middleware')
+const middleware = require('./utils/middleware')
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -18,28 +18,13 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
   })
 
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
-//app.use(middleware.requestLogger)
+app.use(middleware.requestLogger)
   
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+app.use('/api/blogs', blogsRouter)
   
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-  
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-  
-//app.use(middleware.unknownEndpoint)
-//app.use(middleware.errorHandler)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
