e987264650d35ab8171a8c110ed97336ce6be009
Anurag Singh <sa.anuragsingh@gmail.com>
2021-02-24 20:52:35 +0530

    4.13, 4.14 complete

--- Part4/bloglist/controllers/blogs.js
+++ Part4/bloglist/controllers/blogs.js
@@ -7,7 +7,6 @@ blogsRouter.get('/', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response) => {
-
   let blog = request.body
   blog.likes = blog.likes || 0
 
@@ -19,6 +18,25 @@ blogsRouter.post('/', async (request, response) => {
   
   const result = await blogObject.save()
   response.status(201).json(result)
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const body = request.body
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  }
+
+  const result = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+  response.status(200).json(result)
 })
 
 module.exports = blogsRouter
